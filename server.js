5de0f42364b52fea558facb96c2ac856ffe9e1d3
Richard Hill III <rhillz718@gmail.com>
2018-07-29 20:42:06 -0400

    complete api routes for product inventory, began material inventory

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ const connection = sql.createConnection({
 
 connection.connect();
 
-
 app.get('/api/users', (req, res)=>{
     const GET_USERS = "SELECT * FROM users AS Team";
 
@@ -32,7 +31,7 @@ app.get('/api/users', (req, res)=>{
 });
 
 
-//Fetch products api
+//FETCH PRODUCTS API
 app.get('/api/product', (req, res)=>{
     const GET_PRODUCTS = "SELECT * FROM product_inventory";
 
@@ -43,82 +42,121 @@ app.get('/api/product', (req, res)=>{
         res.json(products)
     })
 })
+////////////////////
 
-//Add new product API
+//POST PRODUCT API
+    app.post('/api/product/add', (req, res)=>{
+        
+        const product = {
+            flavor: req.body.flavor,
+            quanity: req.body.quanity
+        }
+        
+        const ADD_PRODUCT = "INSERT INTO product_inventory SET ?";
 
-app.post('/api/product/add', (req, res)=>{
+        connection.query(ADD_PRODUCT, product, (err, data)=>{
+            if(err) throw err;
+            console.log(data)
+        });
 
-console.log(req.body)
-    const product = {
-        flavor: req.body.flavor,
-        quanity: req.body.quanity
-    }
- 
-    const ADD_PRODUCT = "INSERT INTO product_inventory SET ?";
-
-    connection.query(ADD_PRODUCT, product, (err, data)=>{
-        if(err) throw err;
-        console.log(data)
     });
+////////////////
+//UPDATE PRODUCT BY ADDING INVENTORY API
+    app.put('/api/product/update-add', (req, res)=>{
 
-});
+        const flavor = req.body.flavor;
+        const val = req.body.valChanged;
 
-//Update product api
-app.put('/api/product/update-add', (req, res)=>{
+        const UPDATE_PRODUCT1 = "SELECT SUM(quanity + ?) AS newVal FROM product_inventory WHERE flavor = ?";
+        const UPDATE_PRODUCT2 = "UPDATE product_inventory SET quanity = ? WHERE flavor = ?";
 
-    const flavor = req.body.flavor;
-    const val = req.body.valChanged;
-
-    const UPDATE_PRODUCT1 = "SELECT SUM(quanity + ?) AS newVal FROM product_inventory WHERE flavor = ?";
-    const UPDATE_PRODUCT2 = "UPDATE product_inventory SET quanity = ? WHERE flavor = ?";
-
-    connection.query(UPDATE_PRODUCT1, [val, flavor] ,(err, data)=>{
-        if (err) throw err;
-        const newVal = data[0].newVal
-        connection.query(UPDATE_PRODUCT2, [newVal, flavor], (err, data)=>{
-            if(err) throw err;
-            console.log(data)
-            
+        connection.query(UPDATE_PRODUCT1, [val, flavor] ,(err, data)=>{
+            if (err) throw err;
+            const newVal = data[0].newVal
+            connection.query(UPDATE_PRODUCT2, [newVal, flavor], (err, data)=>{
+                if(err) throw err;
+                console.log(data)
+                
+            })
         })
     })
-})
+/////////////////////////////////////
+//UPDATE PRODUCT BY SUBTRACTING INVENTORY
+    app.put('/api/product/update-subtract', (req, res)=>{
 
-//Update product api
-app.put('/api/product/update-subtract', (req, res)=>{
+        const flavor = req.body.flavor;
+        const val = req.body.valChanged;
 
-    const flavor = req.body.flavor;
-    const val = req.body.valChanged;
+        const UPDATE_PRODUCT1 = "SELECT quanity - ? AS newVal FROM product_inventory WHERE flavor = ?";
+        const UPDATE_PRODUCT2 = "UPDATE product_inventory SET quanity = ? WHERE flavor = ?";
 
-    const UPDATE_PRODUCT1 = "SELECT quanity - ? AS newVal FROM product_inventory WHERE flavor = ?";
-    const UPDATE_PRODUCT2 = "UPDATE product_inventory SET quanity = ? WHERE flavor = ?";
-
-    connection.query(UPDATE_PRODUCT1, [val, flavor] ,(err, data)=>{
-        if (err) throw err;
-        const newVal = data[0].newVal
-        connection.query(UPDATE_PRODUCT2, [newVal, flavor], (err, data)=>{
-            if(err) throw err;
-            console.log(data)
-            
+        connection.query(UPDATE_PRODUCT1, [val, flavor] ,(err, data)=>{
+            if (err) throw err;
+            const newVal = data[0].newVal
+            connection.query(UPDATE_PRODUCT2, [newVal, flavor], (err, data)=>{
+                if(err) throw err;
+                console.log(data)
+                
+            })
         })
     })
-})
+/////////////////////////////////////////
+//DELETE PRODUCT API
+    app.delete('/api/product/delete', (req, res)=>{
+        console.log(req.body)
+        const fv = req.body.fv;
+        const DELETE_PRODUCT = "DELETE FROM product_inventory WHERE flavor = ?";
 
-//Delete product api
-app.delete('/api/product/delete', (req, res)=>{
-    console.log(req.body)
-    const fv = req.body.fv;
-    const DELETE_PRODUCT = "DELETE FROM product_inventory WHERE flavor = ?";
-
-    connection.query(DELETE_PRODUCT, [fv], (err, data)=>{
-        if (err) throw err;
-        console.log(data)
+        connection.query(DELETE_PRODUCT, [fv], (err, data)=>{
+            if (err) throw err;
+            console.log(data)
+        })
     })
-})
+/////////////////
+
+//FETCH MATERIAL INVENTORY API
+    app.get('/api/materials', (req, res) => {
+
+        const GET_MATERIAL = "SELECT * FROM material_inventory";
+
+        connection.query(GET_MATERIAL, (err, data)=>{
+            const materials = data;
+            if(err) throw err;
+
+            res.json(materials)
+        })
+    })
+////////////////////////////
+
+//POST MATERIAL INVENTORY API
+    app.post('/api/material/add', (req, res)=>{
+        const stock = {
+            item: req.body.item,
+            quanity: req.body.quanity
+        };
+
+        const NEW_STOCK = "INSERT INTO material_inventory SET ?";
+
+        connection.query(NEW_STOCK, stock, (err, data)=>{
+            if (err) throw err;
+
+            console.log(data)
+        }) 
+    })
+////////////////////////////
+
+
+
+
+
+
+
+
 
 //GET TRANSACTIONS
 app.get('/api/transactions', (req, res)=>{
 
-    const GET_TRANS  = 'SELECT * FROM transaction';
+    const GET_TRANS  = 'SELECT * FROM transactions';
 
     connection.query(GET_TRANS, (err, data)=>{
         if(err) throw err;
@@ -127,7 +165,7 @@ app.get('/api/transactions', (req, res)=>{
 })
 
 //POST TRANSACTION && UPDATE PRODUCT INVENTORY
-app.post('/api/post/transaction', (req, res)=>{
+app.post('/api/post/transactions', (req, res)=>{
     const quanity = req.body.quanity;
     const flavor = req.body.flavor;
     const trans = {
@@ -136,7 +174,7 @@ app.post('/api/post/transaction', (req, res)=>{
         quanity_purchased: req.body.quanity,
         amount_paid: req.body.amount,
     };
-    const NEW_TRANSACTION = 'INSERT INTO transaction SET ?';
+    const NEW_TRANSACTION = 'INSERT INTO transactions SET ?';
     const UPDATE_PRODUCT = "UPDATE product_inventory SET quanity = ? WHERE flavor = ?";
 
     connection.query(NEW_TRANSACTION, trans, (err, data)=>{
